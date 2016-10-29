22b128c69db2d4ddba8333e70d7ccd19cdfed3cd
VengantMjolnir <vengantmjolnir@gmail.com>
2016-10-28 22:00:47 -0700

    NP - Initial commit of project

---
+++ Unity/Assets/Fantasy Environment and Animated Characters (Pixel Art)/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset = new Vector3 (0,0,-10);
+		
+	void Start ()
+	{
+
+	}
+		
+	void Update ()
+	{
+			transform.position = player.transform.position + offset;
+	}
+}
